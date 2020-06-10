4212f045555e0a0ba80e48edca8f3489de3982da
Rahan Bouess <wangleibniz01@gmail.com>
2020-06-10 16:38:11 +0800

    almost  finish seting up the server

---
+++ server/models/Product.js
@@ -0,0 +1,76 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema (
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    image: {
+      type: Array,
+      default: []
+    },
+    title: {
+      type: String,
+      maxLength: 50
+    },
+
+    description: {
+      type: String,
+      maxLength: 150
+    },
+
+    sizes: {
+      type: Array,
+      default: []
+    },
+
+    quantity: {
+      type: Number,
+      default: 1
+    },
+
+    price: {
+      type: Number,
+      default: 0
+    },
+    categories: {
+      type: Array,
+      default: []
+    },
+
+    brand: {
+      type: Array,
+      default: []
+    },
+    
+    color:{
+        type: Array,
+        default: []
+    },
+
+    like: {
+      type: Boolean,
+      default: false
+    },
+  },
+  {timestamp: true}
+);
+
+productSchema.index (
+  {
+    title: 'text',
+    description: 'text'
+  },
+  {
+    weights: {
+      title: 5,
+      description: 1
+    },
+  }
+);
+
+const Product = mongoose.model ('Product', productSchema);
+
+module.exports = {Product};
